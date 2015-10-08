773fa03f6ff30e1357d0d9b7437b8964ec6a67c0
jithin <jroy6@asu.edu>
2015-10-08 11:16:18 -0700

    Added user model classes

--- src/com/ipims/models/User.java
+++ src/com/ipims/models/User.java
@@ -2,4 +2,16 @@ package com.ipims.models;
 
 public class User {
 
+	public enum UserType {
+	    DOCTOR,
+	    PATIENT,
+	    HSPSTAFF,
+	    NURSE,
+	    UNKNOWN
+	}
+
+	public UserType getUsertype() {
+		return UserType.UNKNOWN;
+	}
+	
 }
