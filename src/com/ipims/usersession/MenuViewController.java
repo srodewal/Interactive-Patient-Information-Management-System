b3b0890aecf8024a613278b30b7ca9be620d0050
Gregg <Gregg@Gregg.mobile.asu.edu>
2015-10-08 09:32:02 -0700

    gregg did this

---
+++ src/com/ipims/usersession/MenuViewController.java
@@ -0,0 +1,82 @@
+package com.ipims.usersession;
+
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class MenuViewController {
+
+	
+	
+	private Scene menuScene;
+	
+	public Scene getScene() {
+		GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Menu");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+
+        Button btn = new Button("Schedule Appointment");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+            	
+            	
+            	ScheduleAppointmentViewController schedule = new ScheduleAppointmentViewController();
+            	
+            	Stage stage = (Stage) menuScene.getWindow();
+            	stage.setScene(schedule.getScene());
+            	
+            }
+        });
+        menuScene = new Scene(grid, 300, 275);
+		return menuScene;
+	}
+	
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
