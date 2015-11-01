a973d345b0f3f421b3e066c51ed5c892fdeaaed5
srodewal <srodewal@asu.edu>
2015-11-01 12:02:46 -0700

    view patient info feature updated menu button order updated

--- src/com/ipims/views/ViewPatientInfoView.java
+++ src/com/ipims/views/ViewPatientInfoView.java
@@ -3,6 +3,7 @@ package com.ipims.views;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.ipims.Helper;
 import com.ipims.database.DatabaseManager;
 import com.ipims.healthconditions.HealthViewController; // should be taken out
 import com.ipims.hsp.ViewPatientInfoViewController;
@@ -70,16 +71,6 @@ public class ViewPatientInfoView extends BaseView {
 				}
 			});
 			
-			Button submitBtn = new Button("Submit");
-			submitBtn.setOnAction(new EventHandler<ActionEvent>() {
-
-				@Override
-				public void handle(ActionEvent e) {
-					actionTarget.setFill(Color.GREEN);
-					actionTarget.setText("Submit Button Pressed!");
-					// send to database
-				}
-			});
 			
 			hbox.getChildren().addAll(titleLabel, mainMenuBtn);
 			vbox.getChildren().add(hbox);
@@ -94,6 +85,42 @@ public class ViewPatientInfoView extends BaseView {
 			
 			HBox patientHbox = new HBox();
 			patientHbox.setSpacing(10);
+			
+			Button submitBtn = new Button("Submit");
+			submitBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+				@Override
+				public void handle(ActionEvent e) {
+					actionTarget.setFill(Color.GREEN);
+					actionTarget.setText("Submit Button Pressed!");
+					
+					// clear list
+					items.clear();
+					
+					// get user from database
+					Patient tempPatient = Helper.getPatientAtIndex(patientComboBox.getSelectionModel().getSelectedIndex());
+					User tempUser = DatabaseManager.getInstance().getUser(tempPatient.getUserId());
+					
+					String name = "Name: " + tempUser.getName();
+					items.add(name);
+					String dob = "DOB: " + tempUser.getDateOfBirth();
+					items.add(dob);
+					String address = "Address: " + tempUser.getAddress();
+					items.add(address);
+					String ssn = "SSN: " + tempUser.getSsn();
+					items.add(ssn);
+					String phoneNum = "Phone Number: " + tempUser.getPhoneNumber();
+					items.add(phoneNum);
+					String email = "Email: " + tempUser.getEmail();
+					items.add(email);
+					String healthInsurance = "Health Insurance Provider: " + tempUser.getInsurance();
+					items.add(healthInsurance);
+					String sex = "Sex: " + tempUser.getSex();
+					items.add(sex);
+					String race = "Race: " + tempUser.getRace();
+					items.add(race);
+				}
+			});
 			
 			patientHbox.getChildren().addAll(PatientLabel, patientComboBox, submitBtn);
 			
