205d53095182fbeb52c0aedd23d237b806ecad9b
srodewal <srodewal@asu.edu>
2015-11-01 23:13:10 -0700

    stats reports can be printed

--- src/com/ipims/views/ReportsView.java
+++ src/com/ipims/views/ReportsView.java
@@ -13,6 +13,8 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.print.PrinterJob;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -30,9 +32,30 @@ import javafx.scene.text.Text;
 import javafx.util.Callback;
 
 public class ReportsView extends BaseView {
+	private VBox vbox;
+	
+	// print function to be called for a report
+	private void print(Node node) {
+	    System.out.println("Creating a printer job...");
+
+	    PrinterJob job = PrinterJob.createPrinterJob();
+	    if (job != null) {
+	      System.out.println(job.jobStatusProperty().asString());
+
+	      boolean printed = job.printPage(node);
+	      if (printed) {
+	        job.endJob();
+	      } else {
+	        System.out.println("Printing failed.");
+	      }
+	    } else {
+	      System.out.println("Could not create a printer job.");
+	    }
+	  }
+	
 	public void createReportsView(ReportsViewController parentController, Text reportTitle, ObservableList<String> items) {
 
-		VBox vbox = new VBox();
+		vbox = new VBox();
 		vbox.setPadding(new Insets(25));
 		vbox.setSpacing(8);
 
@@ -122,6 +145,9 @@ public class ReportsView extends BaseView {
 				// to-do
 				actionTarget.setFill(Color.GREEN);
 				actionTarget.setText("Print button pressed!");
+				// make information printable
+				// implementation of print
+				print(vbox);
 			}
 		});
 
