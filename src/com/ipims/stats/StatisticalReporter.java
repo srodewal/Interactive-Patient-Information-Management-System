eb36c4c933aae27ef9b4909686687332a53f975c
Philip Terzic <pterzic@asu.edu>
2015-11-01 17:40:49 -0700

    Stats

--- src/com/ipims/stats/StatisticalReporter.java
+++ src/com/ipims/stats/StatisticalReporter.java
@@ -7,6 +7,7 @@ import java.util.List;
 import com.ipims.database.DatabaseManager;
 import com.ipims.models.Appointment;
 import com.ipims.models.LabRecord;
+ 
 
 public class StatisticalReporter {
 
